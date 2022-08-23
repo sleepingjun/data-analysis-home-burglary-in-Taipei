f880a9f2f491e69782c774fddf24bbdf33f0fc1c
ani <best19980102@gmail.com>
2022-08-23 19:09:36 +0800

    further analysis on top3 dangerous areas

--- main.py
+++ main.py
@@ -14,15 +14,24 @@ print("data.index: ",data.index)
 print("data.col: ",data.columns)
 total=int(len(data))
 
-year=[]
-month=[]
-day=[]
-for i in data["發生日期"]:
-    year.append(str(i)[:-4])
-    month.append(str(i)[-4:-2])
-    day.append(str(i)[-2:])
-    #print(f'year: {year} month: {month} day: {day}')
 
+def split_date(data,date_col):
+    """
+    to split date into year, month, day list
+    :param data: dataframe
+    :param date_col: str
+    :return: list
+    """
+    year=[]
+    month=[]
+    day=[]
+    for i in data[date_col]:
+        year.append(str(i)[:-4])
+        month.append(str(i)[-4:-2])
+        day.append(str(i)[-2:])
+        #print(f'year: {year} month: {month} day: {day}')
+    return year,month,day
+year,month,day=split_date(data,"發生日期")
 data.insert(5,column="year",value=year)
 data.insert(6,column="month",value=month)
 data.insert(7,column="day",value=day)
@@ -75,7 +84,7 @@ ax[2].bar(dict_d.keys(),
           alpha=0.5,
           label="date")
 ax[2].legend()
-plt.show()
+plt.close()
 
 
 # analysis administrative area
@@ -125,3 +134,80 @@ plt.setp(autotexts, size=8, weight="bold")
 
 ax.set_title("Administrative Area Percentage of House Burglary\nTotal: "+str(len(data)))
 plt.close()
+
+#I wanna do further analysis on Zhongshan dist.
+print("dangerous part")
+zs_data=data.loc[(data["administrative area"]=="中山區")]
+sl_data=data.loc[(data["administrative area"]=="士林區")]
+wh_data=data.loc[(data["administrative area"]=="萬華區")]
+
+zs_year,zs_month,zs_day=split_date(zs_data,"發生日期")
+zs_year,zs_month,zs_day=split_date(zs_data,"發生日期")
+zs_year,zs_month,zs_day=split_date(zs_data,"發生日期")
+
+sl_year,sl_month,sl_day=split_date(sl_data,"發生日期")
+sl_year,sl_month,sl_day=split_date(sl_data,"發生日期")
+sl_year,sl_month,sl_day=split_date(sl_data,"發生日期")
+
+wh_year,wh_month,wh_day=split_date(wh_data,"發生日期")
+wh_year,wh_month,wh_day=split_date(wh_data,"發生日期")
+wh_year,wh_month,zs_day=split_date(wh_data,"發生日期")
+
+sl_dict_y=build_dict(sl_year)
+sl_dict_m=build_dict(sl_month)
+sl_dict_d=build_dict(sl_day)
+
+zs_dict_y=build_dict(zs_year)
+zs_dict_m=build_dict(zs_month)
+zs_dict_d=build_dict(zs_day)
+
+wh_dict_y=build_dict(wh_year)
+wh_dict_m=build_dict(wh_month)
+wh_dict_d=build_dict(wh_day)
+
+fig, ax = plt.subplots(3,1)
+
+ax[0].plot([i for i in zs_dict_y.keys()],
+           [i for i in zs_dict_y.values()],
+           '.-',
+           label="Zhong Shan")
+ax[0].plot([i for i in sl_dict_y.keys()],
+           [i for i in sl_dict_y.values()],
+           '.-',
+           label="Shi Lin")
+ax[0].plot([i for i in wh_dict_y.keys()],
+           [i for i in wh_dict_y.values()],
+           '.-',
+           label="Wan Hua")
+ax[0].legend(prop={'size': 6})
+ax[0].set_xlabel("year")
+ax[1].plot([i for i in zs_dict_m.keys()],
+           [i for i in zs_dict_m.values()],
+           '.-',
+           label="Zhong Shan")
+ax[1].plot([i for i in sl_dict_m.keys()],
+           [i for i in sl_dict_m.values()],
+           '.-',
+           label="Shi Lin")
+ax[1].plot([i for i in wh_dict_m.keys()],
+           [i for i in wh_dict_m.values()],
+           '.-',
+           label="Wan Hua")
+ax[1].legend(prop={'size': 6})
+ax[1].set_xlabel("month")
+ax[2].bar(zs_dict_d.keys(),
+           zs_dict_d.values(),
+           alpha=0.5,
+           label="Zhong Shan")
+ax[2].plot([i for i in sl_dict_d.keys()],
+           [i for i in sl_dict_d.values()],
+           '.-',
+           label="Shi Lin")
+ax[2].plot([i for i in wh_dict_d.keys()],
+           [i for i in wh_dict_d.values()],
+           '.-',
+           label="Wan Hua")
+ax[2].legend(prop={'size': 6})
+ax[2].set_xlabel("day")
+plt.tight_layout()
+plt.show()
